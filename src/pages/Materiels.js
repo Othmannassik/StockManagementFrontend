85530703b203fcbccc2d32b5556588e6fb60463f
A.AJDIR <abdelaliajdir@gmail.com>
2023-08-30 16:33:36 +0100

    Update materiels

--- src/pages/Materiels.js
+++ src/pages/Materiels.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import axios from 'axios';
+        
 
 export default function Materiels() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+  
+
   return (
-    <div>Materiels</div>
-  )
+  
+    <div className="card">
+            <DataTable value={posts} tableStyle={{ minWidth: '50rem' }}>
+                <Column field="id" header="Id" sortable style={{ width: '25%' }} />
+                <Column field="title" header="NaTitleme" sortable style={{ width: '25%' }} />
+                <Column field="body" header="Category" sortable style={{ width: '25%' }} />
+            </DataTable>
+        </div>
+    
+  );
 }
